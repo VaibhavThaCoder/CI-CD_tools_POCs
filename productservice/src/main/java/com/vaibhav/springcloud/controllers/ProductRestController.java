75d68f914223cb75ef6a13aab576d813cfa6c3b4
VaibhavThaCoder <vaibhav92jun@gmail.com>
2021-12-27 16:18:17 +0530

    productserviceq

---
+++ productservice/src/main/java/com/vaibhav/springcloud/controllers/ProductRestController.java
@@ -0,0 +1,38 @@
+package com.vaibhav.springcloud.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.vaibhav.springcloud.dto.Coupon;
+import com.vaibhav.springcloud.model.Product;
+import com.vaibhav.springcloud.repos.ProductRepo;
+
+@RestController
+@RequestMapping("/productapi")
+public class ProductRestController {
+
+	@Autowired
+	private ProductRepo repo;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${couponService.url}")
+	private String couponServiceURL;
+
+	@RequestMapping(value = "/products", method = RequestMethod.POST)
+	public Product create(@RequestBody Product product) {
+		Coupon coupon = restTemplate.getForObject(couponServiceURL + product.getCouponCode(), Coupon.class);
+		//discount logic
+		product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
+
+		return repo.save(product);
+
+	}
+
+}
